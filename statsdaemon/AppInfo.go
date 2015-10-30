253ff41b9727d3ad01db9253a9bd8e840e07e7fc
Sam Zaydel <szaydel@gmail.com>
2015-10-30 15:35:49 -0700

    Closing gap between racktop standard for Go apps and statsdaemon.

---
+++ statsdaemon/AppInfo.go
@@ -0,0 +1,18 @@
+//  Copyright 2009-2015 RackTop Systems Inc. and/or its affiliates.
+//  http://www.racktopsystems.com
+//
+//  The methods and techniques utilized herein are considered TRADE SECRETS
+//  and/or CONFIDENTIAL unless otherwise noted. REPRODUCTION or DISTRIBUTION
+//  is FORBIDDEN, in whole and/or in part, except by express written permission
+//  of RackTop Systems.
+//
+// modkernel is a utility used to non-persistently alter values for tunables,
+// which are normally just global variables defined in the OS, using the modular
+// debugger tool.
+
+package main
+
+const APP_NAME = "statsdaemon"
+const APP_DESCRIPTION = "BrickStor Statistics And Metrics Aggrefator"
+const APP_GUID = "2e79857279444474a505f9a224c7245d"
+const APP_VERSION = "1.0.0"
--- statsdaemon/AppMain.go
+++ statsdaemon/AppMain.go
@@ -87,22 +87,6 @@ func sanitizeBucket(bucket string) string {
 	return string(b[:bl])
 }
 
-var (
-	serviceAddress    = flag.String("address", ":8125", "UDP service address")
-	tcpServiceAddress = flag.String("tcpaddr", "", "TCP service address, if set")
-	maxUdpPacketSize  = flag.Int64("max-udp-packet-size", 1472, "Maximum UDP packet size")
-	postFlushCmd      = flag.String("post-flush-cmd", "stdout", "Command to run on each flush")
-	graphiteAddress   = flag.String("graphite", "127.0.0.1:2003", "Graphite service address (or - to disable)")
-	flushInterval     = flag.Int64("flush-interval", 10, "Flush interval (seconds)")
-	debug             = flag.Bool("debug", false, "print statistics sent to graphite")
-	showVersion       = flag.Bool("version", false, "print version string")
-	deleteGauges      = flag.Bool("delete-gauges", true, "don't send values to graphite for inactive gauges, as opposed to sending the previous value")
-	persistCountKeys  = flag.Int64("persist-count-keys", 60, "number of flush-intervals to persist count keys")
-	receiveCounter    = flag.String("receive-counter", "", "Metric name for total metrics received per interval")
-	percentThreshold  = Percentiles{}
-	prefix            = flag.String("prefix", "", "Prefix for all stats")
-	postfix           = flag.String("postfix", "", "Postfix for all stats")
-)
 
 func init() {
 	flag.Var(&percentThreshold, "percent-threshold",
